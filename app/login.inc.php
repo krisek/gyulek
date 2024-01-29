a0def03e940ff649a7fb49f527c6db50f186f7d2
Kristof Szabo <kris@lxsystems.de>
2024-01-29 20:37:33 +0100

    cleanup and fixes

--- app/login.inc.php
+++ app/login.inc.php
@@ -1,31 +1,34 @@
 <?
 
-function pop3_login($host,$port,$user,$pass,$folder="INBOX",$ssl=false){
-    $ssl=($ssl==false)?"/novalidate-cert":"";
-    return (imap_open("{"."$host:$port/pop3$ssl"."}$folder",$user,$pass));
+function pop3_login($host,$user,$pass){
+    return (imap_open("{"."$host:995/pop3/ssl/novalidate-cert"."}",$user,$pass));
 }
 
 
-if( ( !isset($_SESSION[key]) && !isset($_POST[key]) ) || (!isset($_SESSION[user]) && !isset($_POST[user]) && !isset($_COOKIE[client_id])) ){
+if( 
+    ( !isset($_SESSION['key']) && ! isset($_POST['key']) ) || 
+        ( !isset($_SESSION['user']) && !isset($_POST['user']) && !isset($_COOKIE['client_id']) ) || 
+            $_SESSION['loginfail'] == 1){
 ?>
 <form id="login" action="" method="post">
-<input type="hidden" name="uri" value="<?=$_SERVER[REQUEST_URI]?>" >
+<input type="hidden" name="uri" value="<?=$_SERVER['REQUEST_URI']?>" >
 <div id="login_div">
 <center><span style="font-size: 44px;">gyulek.</span><br/>
 
-<?if( (($_POST[user] == "") && !isset($_COOKIE[client_id]) && !isset($_SESSION[user]) )  ){?>
-felhasználónév<?if($_SESSION[loginfail] == 1):?><?endif;?>:<br/>
-<input type="text" name="user" value="" size="12" style="width: 250px" class="lightborder <?if($_SESSION[loginfail] == 1):?>redborder<?endif;?> lightborder_w2" ><br/><br/>
+<?if( ($_POST['user'] == "") && (!isset($_COOKIE['client_id']) && !isset($_SESSION['user']) ) || $_SESSION['loginfail'] == 1 ){?>
+<?if($_SESSION['loginfail'] == 1):?>Sikertelen belépés<br/><?php $_SESSION['loginfail'] = 0?><?endif;?>
+felhasználónév:<br/>
+<input type="text" name="user" value="" size="12" style="width: 250px" class="lightborder <?if($_SESSION['loginfail'] == 1):?>redborder<?endif;?> lightborder_w2" ><br/><br/>
 jelszó:<br/>
 <input type="password" name="password"  style="width: 150px" value="" class="lightborder lightborder_w2" ><br/>
 <br>
-<span style="font-size: 16px;"><input type="checkbox" name="remember" value="remember" checked>remember me<span><br>
+<span style="font-size: 16px;"><input type="checkbox" name="remember" value="remember" checked>emlékezz rám<span><br>
 <?}?>
 
-kulcs:<br/>
-<input type="password" name="key"  style="width: 150px" value="" class="lightborder lightborder_w2" ><br/>
+<!-- kulcs:<br/> //-->
+<input type="hidden" name="key"  style="width: 150px" value="" class="lightborder lightborder_w2" ><br/>
 <br>
-<input type="submit" name="login" value="login" class="lightborder_w2" ><br/>
+<input type="submit" name="login" value="Belépés" class="lightborder_w2" ><br/>
 </form>
 </center>
 </div>
@@ -36,42 +39,55 @@ kulcs:<br/>
     }
 else{
 //do some logic
-    if(isset($_POST[key])){
-        $_SESSION[key] = $_POST[key];
-        }
-    if(isset($_COOKIE[client_id])){
+
+    if(isset($_SESSION['loginfail'])){
+        $_SESSION['loginfail'] = 0;
+    }
+
+    if(isset($_POST['key'])){
+        $_SESSION['key'] = $_POST['key'];
+    }
+    
+    if(isset($_POST['user']) && ! in_array($_POST['user'], $offshore_users)   ){
+        $_SESSION['loginfail'] = 1;
+        error_log('user not on list: ' . $_POST['user'] . "   " . join(', ', $offshore_users));
+        header("Location: " . (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+        die();
+    }
+
+    if(isset($_COOKIE['client_id'])){
         //put username into session
-        $client_id = mysql_real_escape_string($_COOKIE[client_id]);
-        $result = mysqli_query($dbh, "SELECT * FROM ${global_table_prefix}permanent_users WHERE client_id = '$client_id'") or die("Error in statement -- this was not very elegant I know");
+        $client_id = mysqli_real_escape_string($dbh, $_COOKIE['client_id']);
+        $result = mysqli_query($dbh, "SELECT * FROM ${global_table_prefix}permanent_users WHERE client_id = '$client_id'"); 
         if ($line = mysqli_fetch_assoc($result)) {
-            $_SESSION[user] = $line[user];
-            }
-        mysql_free_result($result);
+            $_SESSION['user'] = $line['user'];
         }
-    elseif((isset($_POST[user]) && isset($_POST[password]))){
-        if(isset($_SESSION[loginfail])){
-            unset($_SESSION[loginfail]);
-            }
-            //check password
-        if(pop3_login("mail.lutheran.hu",110,$_POST[user],$_POST[password]) === FALSE){
-            $_SESSION[loginfail] = 1;
-            }
+        mysqli_free_result($result);
+    }
+    elseif(isset($_POST['user']) && isset($_POST['password'])){
+        
+        if(pop3_login("mail.lutheran.hu",$_POST['user'],$_POST['password']) === FALSE){
+            error_log('login failed ' . $_POST['user']);
+            $_SESSION['loginfail'] = 1;
+            header("Location: " . (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+            die();
+        }
         else{
-            $_SESSION[user] = preg_replace('/@lutheran.hu/','',$_POST[user]);
+            $_SESSION['user'] = preg_replace('/@lutheran.hu/','',$_POST['user']);
             
-            if($_POST[remember] == 'remember'){
+            if($_POST['remember'] == 'remember'){
                 //generate id
-                $user = mysql_real_escape_string(strtolower($_SESSION[user]));
+                $user = mysqli_real_escape_string($dbh, strtolower($_SESSION['user']));
                 $client_id = md5(rand());
-                $statement = "INSERT INTO ${global_table_prefix}permanent_users (user, client_id) values ('$user','$client_id')";
-                $result = mysqli_query($dbh, $statement) or die("Error in statement -- this was not very elegant I know");
+                $statement = "INSERT INTO ${global_table_prefix}permanent_users (user, client_id, ts) values ('$user','$client_id', NOW())";
+                $result = mysqli_query($dbh, $statement) or die("Error in statement -- " . mysqli_error($dbh));
                 //setcookie("client_id", $client_id, time()+60*60*24*28,null,null,TRUE);
                 setcookie("client_id", $client_id, time()+60*60*24*28);
-                }
             }
         }
-
     }
+
+}
 
 
 ?>
